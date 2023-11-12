7641dc7e12cb09baa0534904e1dcd6f3ce3c8489
V-Mokhun <mohunmax1408@gmail.com>
2023-11-12 16:03:52 +0100

    Add react integration and shadcnui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig, passthroughImageService } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    ,
+    react(),
+  ],
   image: {
     service: passthroughImageService(),
   },
