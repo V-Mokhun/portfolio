fa5a44f52a001cb498987291841217a186cabc46
V-Mokhun <mohunmax1408@gmail.com>
2023-11-24 13:13:18 +0100

    Add headerMobileMenu component to work and look properly

--- src/ui/widgets/header/header-mobile-menu.tsx
+++ src/ui/widgets/header/header-mobile-menu.tsx
@@ -1,18 +1,25 @@
 import { Sheet, SheetContent, SheetTrigger } from "@/ui/shared";
 import { HeaderNav } from "./header-nav";
+import { useState } from "react";
 
 interface HeaderMobileMenuProps {}
 
 export const HeaderMobileMenu = ({}: HeaderMobileMenuProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={(open) => setIsOpen(open)}>
       <SheetTrigger className="md:hidden">
         <svg className="w-8 h-8 fill-primary">
           <use xlinkHref="/sprite.svg#hamburger"></use>
         </svg>
       </SheetTrigger>
       <SheetContent className="md:hidden" side="left">
-        <HeaderNav />
+        <HeaderNav
+          onLinkClick={() => {
+            setIsOpen(false);
+          }}
+        />
       </SheetContent>
     </Sheet>
   );
--- src/ui/widgets/header/header-nav.tsx
+++ src/ui/widgets/header/header-nav.tsx
@@ -4,19 +4,28 @@ interface HeaderNavProps {
   navClassName?: string;
   listClassName?: string;
   linkClassName?: string;
+  onLinkClick?: () => void;
 }
 
-export const HeaderNav = ({ navClassName, linkClassName, listClassName }: HeaderNavProps) => {
+export const HeaderNav = ({
+  navClassName,
+  linkClassName,
+  listClassName,
+  onLinkClick,
+}: HeaderNavProps) => {
   return (
-    <nav className={cn("flex justify-center flex-1", navClassName)}>
+    <nav
+      className={cn("flex md:justify-center flex-1 py-4 md:py-0", navClassName)}
+    >
       <ul
         className={cn(
-          "flex items-center gap-4 text-white font-medium text-xl",
+          "flex flex-col md:flex-row md:items-center gap-6 md:gap-4 text-white font-medium text-xl",
           listClassName
         )}
       >
         <li>
           <a
+            onClick={onLinkClick}
             className={cn(
               "transition-colors hover:text-primary p-2",
               linkClassName
@@ -28,6 +37,7 @@ export const HeaderNav = ({ navClassName, linkClassName, listClassName }: Header
         </li>
         <li>
           <a
+            onClick={onLinkClick}
             className={cn(
               "transition-colors hover:text-primary p-2",
               linkClassName
@@ -39,6 +49,7 @@ export const HeaderNav = ({ navClassName, linkClassName, listClassName }: Header
         </li>
         <li>
           <a
+            onClick={onLinkClick}
             className={cn(
               "transition-colors hover:text-primary p-2",
               linkClassName
@@ -46,6 +57,18 @@ export const HeaderNav = ({ navClassName, linkClassName, listClassName }: Header
             href="#projects"
           >
             Projects
+          </a>
+        </li>
+        <li className="md:hidden">
+          <a
+            onClick={onLinkClick}
+            className={cn(
+              "transition-colors hover:text-primary p-2",
+              linkClassName
+            )}
+            href="#contact"
+          >
+            Contact
           </a>
         </li>
       </ul>
