026d125a025d92a19d52a52a452a1150f4d9db31
V-Mokhun <mohunmax1408@gmail.com>
2023-11-24 13:53:06 +0100

    Redesign title and fix header bug

--- src/ui/shared/primitives/title.tsx
+++ src/ui/shared/primitives/title.tsx
@@ -17,9 +17,10 @@ export const Title = ({
   return (
     <Component
       className={cn(
-        "font-bold mb-6 md:mb-8",
-        size === "sm" && "text-xl sm:text-2xl md:text-3xl mb-4 md:mb-6",
-        size === "md" && "text-4xl xl:text-5xl",
+        "font-bold mb-4 md:mb-6",
+        size === "sm" && "text-xl sm:text-2xl md:text-3xl",
+        size === "md" &&
+          "text-4xl xl:text-5xl pb-3 md:pb-5 relative after:block after:h-0.5 after:bg-primary after:w-20 after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2",
         size === "lg" && "text-4xl md:text-5xl xl:text-6xl",
         className
       )}
