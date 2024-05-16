272b06afc3b836156d7a7917c70fac1aef483009
V-Mokhun <mohunmax1408@gmail.com>
2024-05-16 19:26:08 +0200

    chore: remove sentry integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,7 @@ import react from "@astrojs/react";
 import astroI18next from "astro-i18next";
 import { defineConfig, passthroughImageService } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-import sentry from "@sentry/astro";
-import { loadEnv } from "vite";
 import mdx from "@astrojs/mdx";
-
-const { SENTRY_DSN, SENTRY_AUTH_TOKEN } = loadEnv(
-  process.env.NODE_ENV,
-  process.cwd(),
-  ""
-);
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,19 +14,7 @@ export default defineConfig({
     }),
     react(),
     astroI18next(),
-    sentry({
-      environment: import.meta.env.PROD ? "production" : "development",
-      enabled: import.meta.env.PROD,
-      dsn: SENTRY_DSN,
-      sourceMapsUploadOptions: {
-        project: "javascript-astro",
-        authToken: SENTRY_AUTH_TOKEN,
-      },
-      tracesSampleRate: 0.1,
-    }),
-    mdx({
-      
-    }),
+    mdx({}),
   ],
   image: {
     service: passthroughImageService(),
