3e3a23b70bff2050ce890796f04dc10941088cf0
v-mokhun <v.mokhun@gmail.com>
2024-12-08 17:45:45 +0100

    feat: external link in blog; blockquote styles; new post

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.v-mokhun.com",
   output: "hybrid",
   integrations: [
     tailwind({
