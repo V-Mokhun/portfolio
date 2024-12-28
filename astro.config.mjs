6ce1046250e615aa51b975359dc1401d11ba3c99
v-mokhun <v.mokhun@gmail.com>
2024-12-28 17:45:37 +0100

    feat: new blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import astroI18next from "astro-i18next";
 import { defineConfig, passthroughImageService } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
